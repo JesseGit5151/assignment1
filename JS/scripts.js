2226f32a3c6fefd8fc0bcf2bb4ffd4a2aeb94232
JesseGit5151 <jrodriguez5151yahoo.com>
2020-02-05 13:13:33 -0800

    finished assignment

---
+++ JS/scripts.js
@@ -0,0 +1,180 @@
+//STEP 1
+/*var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;*/
+
+//STEP 2
+/*
+2;
+"Jesse";
+true;
+let people = null;
+*/
+
+//STEP 3
+/*
+let problem = 3 * numTotal;
+let expression = firstName + lastName;
+*/
+
+//Step 4
+/*
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+*/
+
+//step 5
+/*
+var firstName;
+firstName = true
+let lastName, address, city, state, zipCode, yourAge;
+const referralSource = 'internet';
+let mayWeContactYou;
+mayWeContactYou = 'no';
+*/
+
+//Step 6
+/*
+let practice = 2 + "everybody";
+console.log(practice);
+*/
+
+//Step 7
+/*
+let firstExample = true + 'Hello';
+console.log(firstExample);
+let secondExample = 2 + false;
+console.log(secondExample);
+*/
+
+//Step 8
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+//Step 9
+/*
+let val = null;
+console.log(val);
+let x;
+console.log(x);
+*/
+
+//Step 10
+/*
+console.log(typeof "Jesse");
+console.log(typeof 12);
+console.log(typeof true);
+console.log(typeof []);
+console.log(typeof x);
+*/
+
+//Step 11
+/*
+alert('Hello ' + 'Jesse Rodriguez' + ', welcome to the JavaScript class!')
+*/
+
+//Step 12
+/*
+let name = "Jesse Rodriguez";
+alert('Hello ' + name + ', welcome to the JavaScript class!')
+*/
+
+//Step 13
+/*
+let course = "Javascript";
+alert(course);
+*/
+
+//Step 14
+/*
+let course = "Javascript";
+let name = "Jesse Rodriguez";
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+*/
+
+//Step 15
+/*
+let name = prompt("What is your name?");
+*/
+
+//Step 16
+/*
+let course = prompt("What class are you taking?");
+*/
+
+//Step 17
+/*
+var x;
+x = 10;
+var y;
+y = 20;
+console.log(x + y);
+*/
+
+//Step 18
+/*
+let x = 20;
+console.log(x + 20);
+*/
+
+//Step 19
+/*
+let x = 20 * 5;
+console.log(x);
+*/
+
+//Step 20
+/*
+let x = 20 % 3;
+console.log(x);
+*/
+
+//Step 21
+/*
+let x = 3;
+let y = 3;
+if (x === y && x > 0) {
+    console.log(true);
+}
+*/
+
+//Step 22
+/*
+let x = 5;
+let y = 3;
+if (x > y || y > 4) {
+    console.log(false);
+}
+*/
+
+//Step 23
+/*
+let widget = new Object();
+console.log(typeof widget);
+*/
+
+//Step 24
+/*
+let widget = {
+     make: "ford"
+};
+console.log(widget instanceof Object);
+*/
+
+//Step 25
+
+let widget = {
+    make: "ford"
+};
+console.log(widget instanceof Array);
